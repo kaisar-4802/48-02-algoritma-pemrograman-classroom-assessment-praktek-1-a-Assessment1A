f77fa325f6e60ed7dd847dd8e71aaed1331deb5e
kaisar rozan b <kaisarrozanbasalika891@gmail.com>
2024-10-24 12:39:36 +0700

    Update Nomor2.java

--- Nomor2.java
+++ Nomor2.java
@@ -1,5 +1,20 @@
 public class Nomor2{
     public static void main(String[] args){
+          
+        String driver = ("mereka");
 
+        switch (driver) {
+            case "Beliau":
+                System.out.println(" Rp25.000");
+                break;
+            case "Mereka":
+                System.out.println("Rp30.000");
+                break;
+            case "Kita":
+                System.out.println("Rp20.000");
+                break;
+                default:
+                System.out.println(" tidak tersedia.");
+        }
     }
 }
