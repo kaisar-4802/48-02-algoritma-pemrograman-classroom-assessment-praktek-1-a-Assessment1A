08de069efdc67f40f3be921c338f9de9996f97ec
kaisar rozan b <kaisarrozanbasalika891@gmail.com>
2024-10-24 12:59:52 +0700

    Update Nomor1.java

--- Nomor1.java
+++ Nomor1.java
@@ -1,15 +1,5 @@
 public class Nomor1{
     public static void main(String[] args){
-//     INPUT total_jam_lembur
-//     IF total_jam_lembur <= 16 THEN
-//         honor_lembur = total_jam_lembur * 10000
-//     ELSE
-//         honor_lembur = (16 * 10000) + ((total_jam_lembur - 16) * 15000)
-//     END IF
-//     OUTPUT "Total jam lembur: " + total_jam_lembur
-//     OUTPUT "Honor lembur: " + honor_lembur
-// END
-
             int jamLembur =(28); 
         // Variabel untuk menyimpan honor lembur
         double honorLembur = 0;
